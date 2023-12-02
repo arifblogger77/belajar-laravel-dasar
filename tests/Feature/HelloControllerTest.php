799f6f73da28dd268a6565b8a55e73a019bb20a3
arifblogger77 <arifblogger77@gmail.com>
2023-12-02 12:15:41 +0700

    Controller

---
+++ tests/Feature/HelloControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HelloControllerTest extends TestCase
+{
+    public function testController()
+    {
+
+        $this->get('/controller/hello/Eko')
+            ->assertSeeText('Halo Eko');
+    }
+}
